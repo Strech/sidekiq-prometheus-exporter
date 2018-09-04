8d4df31b7851b27bc826811d541e60be83b03abf
Sergey Fedorov <oni.strech@gmail.com>
2018-09-04 10:28:53 +0200

    Add new metric "sidekiq_queue_size"
    * Calculated as amount of jobs pushed to the queue.

--- spec/sidekiq/prometheus/exporter_spec.rb
+++ spec/sidekiq/prometheus/exporter_spec.rb
@@ -40,6 +40,11 @@ sidekiq_dead_jobs 4
 # TYPE sidekiq_queue_latency_seconds gauge
 sidekiq_queue_latency_seconds{name="default"} 24.321
 sidekiq_queue_latency_seconds{name="additional"} 1.002
+
+# HELP sidekiq_queue_size The amount of jobs pushed to the queue.
+# TYPE sidekiq_queue_size gauge
+sidekiq_queue_size{name="default"} 1
+sidekiq_queue_size{name="additional"} 0
       TEXT
       # rubocop:enable Layout/IndentHeredoc
     end
