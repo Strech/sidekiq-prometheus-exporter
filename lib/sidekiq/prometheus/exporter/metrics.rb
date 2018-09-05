909030d656676ef59a2ad8565e22bc8801f1a3c1
Tom Mestdagh <mestdagh.tom@gmail.com>
2018-09-05 12:56:05 +0200

    Enclosed Metrics in Sidekiq::Prometheus module.

--- lib/sidekiq/prometheus/exporter/metrics.rb
+++ lib/sidekiq/prometheus/exporter/metrics.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
 
 require 'sidekiq/api'
+module Sidekiq
+  module Prometheus
+    class Metrics
+      QueueStats = Struct.new(:name, :size, :latency)
 
-class Metrics
-  QueueStats = Struct.new(:name, :size, :latency)
+      def initialize
+        @overview_stats = Sidekiq::Stats.new
+        @queues_stats = queues_stats
+      end
 
-  def initialize
-    @overview_stats = Sidekiq::Stats.new
-    @queues_stats = queues_stats
-  end
+      def __binding__
+        binding
+      end
 
-  def __binding__
-    binding
-  end
+      private
 
-  private
-
-  def queues_stats
-    Sidekiq::Queue.all.map do |queue|
-      QueueStats.new(queue.name, queue.size, queue.latency)
+      def queues_stats
+        Sidekiq::Queue.all.map do |queue|
+          QueueStats.new(queue.name, queue.size, queue.latency)
+        end
+      end
     end
   end
 end
