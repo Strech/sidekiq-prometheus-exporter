89647d84fb41b8605378fdcaf931916c6ea3e007
Sergey Fedorov <oni.strech@gmail.com>
2019-04-21 19:45:02 +0200

    Change Standard exporter interface
    * Disable TravisCI notifications

--- lib/sidekiq/prometheus/exporter.rb
+++ lib/sidekiq/prometheus/exporter.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'erb'
 require 'sidekiq/prometheus/exporter/version'
 require 'sidekiq/prometheus/exporter/standard'
 
--- lib/sidekiq/prometheus/exporter/standard.rb
+++ lib/sidekiq/prometheus/exporter/standard.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'erb'
 require 'sidekiq/api'
 
 module Sidekiq
