cec537ef857ec7f85b2e242b3e6c71b5ddcdf714
Sergey Fedorov <oni.strech@gmail.com>
2021-12-14 22:27:52 +0100

    Replace queue workers stats with a single method

--- lib/sidekiq/prometheus/exporter/standard.rb
+++ lib/sidekiq/prometheus/exporter/standard.rb
@@ -10,6 +10,8 @@ module Sidekiq
         TEMPLATE = ERB.new(File.read(File.expand_path('templates/standard.erb', __dir__)))
 
         QueueStats = Struct.new(:name, :size, :latency)
+        QueueWorkersStats = Struct.new(:total_workers, :busy_workers, :processes)
+        WorkersStats = Struct.new(:total_workers, :by_queue)
 
         def self.available?
           true
@@ -18,9 +20,8 @@ module Sidekiq
         def initialize
           @overview_stats = Sidekiq::Stats.new
           @queues_stats = queues_stats
+          @workers_stats = workers_stats
           @max_processing_times = max_processing_times
-          @total_workers = total_workers
-          @workers_stats_by_queue = workers_stats_by_queue
         end
 
         def to_s
@@ -35,8 +36,24 @@ module Sidekiq
           end
         end
 
+        def workers_stats
+          workers_stats = WorkersStats.new(0, {})
+
+          Sidekiq::ProcessSet.new.each_with_object(workers_stats) do |process, stats|
+            stats.total_workers += process['concurrency'].to_i
+
+            process['queues'].each do |queue|
+              stats.by_queue[queue] ||= QueueWorkersStats.new(0, 0, 0)
+              stats.by_queue[queue].processes += 1
+              stats.by_queue[queue].busy_workers += process['busy'].to_i
+              stats.by_queue[queue].total_workers += process['concurrency'].to_i
+            end
+          end
+        end
+
         def max_processing_times
           now = Time.now.to_i
+
           Sidekiq::Workers.new
             .map { |_, _, execution| execution }
             .group_by { |execution| execution['queue'] }
@@ -44,27 +61,6 @@ module Sidekiq
               oldest_execution = executions.min_by { |execution| execution['run_at'] }
               memo[queue] = now - oldest_execution['run_at']
             end
-        end
-
-        def total_workers
-          sidekiq_processes.sum { |process| process['concurrency'].to_i }
-        end
-
-        def workers_stats_by_queue
-          sidekiq_processes.each_with_object({}) do |process, by_queue|
-            process['queues'].each do |queue|
-              by_queue[queue] ||= { 'workers' => 0, 'processes' => 0, 'busy_workers' => 0 }
-              by_queue[queue]['workers'] += process['concurrency']
-              by_queue[queue]['busy_workers'] += process['busy']
-              by_queue[queue]['processes'] += 1
-            end
-
-            by_queue
-          end
-        end
-
-        def sidekiq_processes
-          @sidekiq_processes ||= Sidekiq::ProcessSet.new
         end
       end
     end
