ee91bd30ef32e251c7d81464a16238f9c6beb259
Sergey Fedorov <oni.strech@gmail.com>
2022-12-20 22:38:35 +0100

    Update Docker to ruby 2.7.7 with patched alpine

--- lib/sidekiq/prometheus/exporter/version.rb
+++ lib/sidekiq/prometheus/exporter/version.rb
@@ -4,7 +4,7 @@ module Sidekiq
   module Prometheus
     module Exporter
       VERSION = '0.1.17'.freeze
-      DOCKER_PATCH_VERSION = '1'.freeze
+      DOCKER_PATCH_VERSION = '2'.freeze
     end
   end
 end
