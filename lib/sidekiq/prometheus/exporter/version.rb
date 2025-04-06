50d0ca0cbac8c55f26dd89a92b97e20411e8e0fc
Sergey Fedorov <oni.strech@gmail.com>
2025-04-06 13:35:40 +0200

    Version bump to 0.3.0
    * Update test versions matrix

--- lib/sidekiq/prometheus/exporter/version.rb
+++ lib/sidekiq/prometheus/exporter/version.rb
@@ -5,7 +5,7 @@ module Sidekiq
     module Exporter
       # NOTE: Every version update dropds Docker patch version to 0
       #       and every adjustment in Docker setup bumps it to +1
-      VERSION = '0.2.1'.freeze
+      VERSION = '0.3.0'.freeze
       DOCKER_PATCH_VERSION = '0'.freeze
     end
   end
